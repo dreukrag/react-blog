1e75ab03ad3a2f48ec4ec00829430720953944d9
Filipe Groh <filipegroh@gmail.com>
2020-04-29 13:12:14 -0300

    singup functions

--- components/auth/SignupComponent.js
+++ components/auth/SignupComponent.js
@@ -1,27 +1,45 @@
 import { useState } from "react";
-
+import { singup } from "actions/auth";
+import { signup } from "../../actions/auth";
 const SignupComponent = (props) => {
   const [formValues, setFormValues] = useState({
     name: "teste",
     email: "teste@gmail.com",
     password: "1234",
   });
-  const [error, setError] = useState("");
+  const [error, setError] = useState(false);
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
   const [showForm, setShowForm] = useState(true);
 
+  const resetError = () => {
+    setError(false);
+    setMessage(false);
+  };
+
+  const resetFormValues = () => {
+    setFormValues({ name: "", email: "", password: "" });
+  };
   const { name, email, password } = formValues;
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Submitting", formValues);
-  };
+    setError(false);
+    setLoading(true);
+    signup(formValues).then((data) => {
+      console.log(data)
+      if (data.error) {
+        setError(data.error);
+      } else {
+        resetFormValues();
+        setMessage(data.message);
+        setShowForm(false);
+      }
 
-  const resetError = () => {
-    setError(false)
-    setMessage(false)
-  }
+      setLoading(false);
+    });
+  };
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -31,44 +49,59 @@ const SignupComponent = (props) => {
     resetError();
   };
 
-  const singupForm = () => (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <input
-          className="form-control"
-          name="name"
-          value={name}
-          type="text"
-          placeholder="Type your name"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-group">
-        <input
-          className="form-control"
-          name="email"
-          value={email}
-          type="text"
-          placeholder="Type your email"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-group">
-        <input
-          className="form-control"
-          name="password"
-          value={password}
-          type="password"
-          placeholder="Type your password"
-          onChange={handleChange}
-        />
-      </div>
-      <button className="btn btn-primary" type="submit">
-        Singup!
-      </button>
-    </form>
+  const showLoading = () =>
+    loading ? <div className="alert alert-info">Loading...</div> : null;
+  const showError = () =>
+    error ? <div className="danger alert-danger">{error}</div> : null;
+  const showMessage = () =>
+    message ? <div className="alert alert-info">{message}</div> : null;
+
+  const singupForm = () =>
+    showForm ? (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            className="form-control"
+            name="name"
+            value={name}
+            type="text"
+            placeholder="Type your name"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            name="email"
+            value={email}
+            type="text"
+            placeholder="Type your email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            name="password"
+            value={password}
+            type="password"
+            placeholder="Type your password"
+            onChange={handleChange}
+          />
+        </div>
+        <button className="btn btn-primary" type="submit">
+          Singup!
+        </button>
+      </form>
+    ) : null;
+  return (
+    <>
+      {showError()}
+      {showLoading()}
+      {showMessage()}
+      {singupForm()}
+    </>
   );
-  return <>{singupForm()}</>;
 };
 
 export default SignupComponent;
