dc7883d39054cb1c4dfeec8b4eff6b73a7921b5c
Filipe Groh <filipegroh@gmail.com>
2020-04-29 12:40:25 -0300

    singuppage

--- components/auth/SignupComponent.js
+++ components/auth/SignupComponent.js
@@ -1,35 +1,74 @@
-const SignupComponent = props => {
-  const handleSubmit = e => {
+import { useState } from "react";
+
+const SignupComponent = (props) => {
+  const [formValues, setFormValues] = useState({
+    name: "teste",
+    email: "teste@gmail.com",
+    password: "1234",
+  });
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [showForm, setShowForm] = useState(true);
+
+  const { name, email, password } = formValues;
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Submitting');
+    console.log("Submitting", formValues);
   };
 
-  const handleChange = e => {
-    console.log(e.target.value);
+  const resetError = () => {
+    setError(false)
+    setMessage(false)
+  }
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const n = e.target.name;
+    const v = e.target.value;
+    setFormValues((old) => ({ ...old, [n]: v }));
+    resetError();
   };
+
   const singupForm = () => (
     <form onSubmit={handleSubmit}>
+      <div className="form-group">
         <input
-        name='name'
-        type='text'
-        placeholder='Type your name'
-        onChange={handleChange}
-      />
-      <input
-        name='email'
-        type='text'
-        placeholder='Type your email'
-        onChange={handleChange}
-      />
-      <input
-        name='password'
-        type='password'
-        placeholder='Type your email'
-        onChange={handleChange}
-      />
+          className="form-control"
+          name="name"
+          value={name}
+          type="text"
+          placeholder="Type your name"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <input
+          className="form-control"
+          name="email"
+          value={email}
+          type="text"
+          placeholder="Type your email"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <input
+          className="form-control"
+          name="password"
+          value={password}
+          type="password"
+          placeholder="Type your password"
+          onChange={handleChange}
+        />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Singup!
+      </button>
     </form>
   );
-  return singupForm();
+  return <>{singupForm()}</>;
 };
 
 export default SignupComponent;
